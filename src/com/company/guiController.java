8b39c19adba674d1ae9fca7a376b19cefa1b782a
Dev Kathuria <dk382@snu.edu.in>
2019-09-07 22:06:03 +0530

    Major Update1

--- src/com/company/guiController.java
+++ src/com/company/guiController.java
@@ -2,25 +2,23 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
 
-class guiController extends JFrame {
+class guiController extends JFrame implements Runnable{
 /*
 This class handle GUI related functions and also allows GUI refresh whenever any move is taken by AI
  */
     private JPanel grid = new JPanel(new GridLayout(9, 9)); //Grid layout
-    private JPanel buttons = new JPanel(new GridLayout(2, 1));
+    private JPanel buttons = new JPanel(new GridLayout(1, 2));
     private JTextField[][] fieldArray = new JTextField[9][9]; //array of fields for easy extraction
 
-    private sudokuInitializer sud = new sudokuInitializer(); //initializes array from file
+    sudokuInitializer sud = new sudokuInitializer(); //initializes array from file
     private int flag;
 
-    guiController() {
+    public void run() {
         flag = chooseInputStream(); //choose input method
         guiInit();//Initializes GUI
         //sud.printArray();
+        setVisible(true);
     }
 /*
 Function to display GUI for the user to choose input method method
@@ -31,7 +29,6 @@ File or Manual
         int response = JOptionPane.showOptionDialog(null, "Choose one way", "",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
                 null, options, options[0]);
-        System.out.println(response);
         if (response == 0) {
             try {
                 sud.fileInput();
@@ -73,28 +70,58 @@ Initializes GUI with the given content from the array
         }
         add(grid);
         JButton solve = new JButton("Solve");//Button to compute
+        JButton Clear = new JButton("Clear");//Button to compute
         solve.setBorder(null);
         solve.setBackground(Color.white);
         buttons.add(solve);
-        buttons.add(new JLabel());
         buttons.setBackground(Color.white);
         add(buttons, BorderLayout.SOUTH);
         solve.addActionListener(e -> { //If the button is pressed
             if (flag != 0) {
                 try {
                     getText(); //takes in text from the GUI
-                    sud.printArray(); //debugging purpose
-                    solve.setEnabled(false);//cell value becomes fixed
                 } catch (Exception ex) {
                     JOptionPane.showMessageDialog(null,"Invalid Input");//Throws error if the input is invalid
+                    clearGUI();
+                    flag=-1;
                 }
             }
+            if (flag!=-1) {
+                sud.printArray(); //debugging purpose
+                solve.setEnabled(false);//cell value becomes fixed
+                solver solver = new solver();
+                Thread t2 = new Thread(solver);
+                t2.start();
+                System.out.println("Solving...");
+                Clear.setEnabled(false);
+            }
         });
+
+
+        Clear.setBorder(null);
+        Clear.setBackground(Color.white);
+        buttons.add(Clear);
+        Clear.addActionListener(e -> { //If the button is pressed
+            clearGUI();
+
+        });
+
+        buttons.setPreferredSize(new Dimension(400,50));
         //Basic Frame parameters are set below this
         setSize(400, 500);
         //setVisible(true);//done in Main function
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+    }
+    private void clearGUI(){
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 9; j++) {
+                Main.sudoku_array[i][j] = 0;
+                fieldArray[i][j].setEditable(true);
+                fieldArray[i][j].setText("");
+            }
+        Main.points.clear();
+        refreshGUI();
     }
 /*
 takes in valid input and sets the GUI cells as non editable
@@ -105,23 +132,24 @@ takes in valid input and sets the GUI cells as non editable
             for (int j = 0; j < 9; j++) {
                     temp = fieldArray[i][j].getText();
                     if (!temp.equals("")) {
-                            Main.sudoku_array[i][j] = sud.checkValidInput(temp); //throws exception
-                            fieldArray[i][j].setEditable(false);
-                    }
+                        if(sud.checkValidInput(temp+""))
+                            Main.sudoku_array[i][j] = Integer.parseInt(temp + "");//checks valid input, catches exception
+                        fieldArray[i][j].setEditable(false);
+                    }else
+                        Main.points.add(new Point(i, j));
             }
         }
+        sud.printPoints();
     }
 /*
 Refreshes the GUI whenever AI makes some changes
  */
-    void refreshGUI(){
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (Main.sudoku_array[i][j] != 0) {
-                    fieldArray[i][j].setText(Main.sudoku_array[i][j]+"");
-                }
-            }
-        }
+    protected void refreshGUI(){
+        for (int i=0; i<Main.points.size();i++)
+            if (Main.sudoku_array[(int)Main.points.get(i).getX()][(int)Main.points.get(i).getY()]!=0)
+                fieldArray[(int)Main.points.get(i).getX()][(int)Main.points.get(i).getY()].setText(Main.sudoku_array[(int)Main.points.get(i).getX()][(int)Main.points.get(i).getY()]+"");
+            else
+                fieldArray[(int)Main.points.get(i).getX()][(int)Main.points.get(i).getY()].setText("");
     }
 
 }
--- src/com/company/sudokuInitializer.java
+++ src/com/company/sudokuInitializer.java
@@ -2,16 +2,18 @@ package com.company;
 
 import javax.swing.*;
 import java.io.*;
+import java.awt.Point;
 
 class sudokuInitializer {
+
     /*
     Function to check whether the input given via file or manually is valid or not
     Thorws exception for Invalid Input
      */
-    int checkValidInput(String input) throws Exception{
+    boolean checkValidInput(String input) throws Exception{
         int n = Integer.parseInt(input+"");
         if (n<10 && n>-1)
-            return n;
+            return true;
         else throw new Exception();
     }
     /*
@@ -25,6 +27,9 @@ class sudokuInitializer {
             for (int j = 0; j < 9; j++)
                 System.out.print(Main.sudoku_array[i][j] + " ");
         }
+    }
+    void printPoints(){
+        System.out.println(Main.points);
     }
     /*
     Takes input from file using Jfilechooser, and the input from the file is inserted in the array
@@ -44,7 +49,11 @@ class sudokuInitializer {
                 st.insert(0,stt);
                 for (int j=0;j<st.length();j++){
                     try {
-                        Main.sudoku_array[i][j] = checkValidInput(st.charAt(j)+""); //checks valid input, catches exception
+                        if(checkValidInput(st.charAt(j)+"")) {
+                            Main.sudoku_array[i][j] = Integer.parseInt(st.charAt(j) + "");//checks valid input, catches exception
+                        }
+                        if (Integer.parseInt(st.charAt(j) + "")==0)
+                            Main.points.add(new Point(i, j));
                     }catch (Exception E){
                         JOptionPane.showMessageDialog(null,"Error in extracting data from file\nExiting...");
                         System.exit(1);
@@ -55,6 +64,7 @@ class sudokuInitializer {
             }
         }
     }
+
     /*
 
     //NOTE : Method only to be used in catastrophic situations only, used to take in console input
